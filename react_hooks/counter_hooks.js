f208d5ab23f10b03cea8e0a46efdf8e53ac9cfed
John Kim <jkim82092@gmail.com>
2019-05-21 00:36:51 -0400

    Differentiate between setState and useState

--- react_hooks/counter_hooks.js
+++ react_hooks/counter_hooks.js
@@ -1,8 +1,27 @@
-function CounterHooks() { 
+// function CounterHooks() { 
+//     const [count, setCount] = useState(0); // useState(0) -> holds 0 as the initial value
+
+//     const handleClick = () => {
+//         setCount(count + 1);
+//     };
+
+//     return (
+//         <div>
+//             <h3 className="center">Welcome to the Counter of Life </h3>
+//             <button
+//                 className="center-block"
+//                 onClick={handleClick}> {count} </button>
+//         </div>
+//     );
+// };
+
+function CounterHooks() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(new Date());
 
     const handleClick = () => {
         setCount(count + 1);
+        setTime(new Date());
     };
 
     return (
@@ -10,7 +29,23 @@ function CounterHooks() {
             <h3 className="center">Welcome to the Counter of Life </h3>
             <button
                 className="center-block"
-                onClick={handleClick}> {count} </button>
+                onClick={handleClick}>{count}</button>
+            <p className="center">
+                at: {`${time.getHours()} : ${time.getMinutes()} : ${time.getSeconds()}`}</p>
         </div>
     );
-};
+}
+
+//  unlike setState calls, the values passed into useState replaces the state value. 
+// setState merges object properties but useState replaces the entire value.
+
+// 🐢 merge (setState) vs replace (useState)
+// assume initial state is {name: "Ohans"}
+
+// setState({ age: "unknown" }) // =>
+// new state object will be 
+// {name: "Ohans", age: "unknown"}
+
+// useState({ age: "unknown" }) // =>
+// new state object will be
+// {age: "unknown"} - initial object is replaced
