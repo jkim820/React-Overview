3f0e2de9b4405584628c9d9646e63836de2934e1
John Kim <jkim82092@gmail.com>
2019-06-07 23:59:02 -0400

    Comment out notes

--- react_medium/programming.js
+++ react_medium/programming.js
@@ -14,4 +14,34 @@ for (let i = 0; i < numbers.length; i++) {
     doubleWithImp.push(numberdouble)
 }
 
-console.log(doubleWithImp)
+console.log(doubleWithImp);
+
+// Functional Programming
+
+let student = {
+    firstName: "testing",
+    lastName: "testing",
+    marks: 500
+}
+
+// impure function
+function appendAddress() {
+    student.address = { streetNumber: "0000", streetName: "first", city: "somecity" };
+}
+
+console.log(appendAddress());
+
+// pure function
+function appendAddress(student) {
+    let copystudent = Object.assign({}, student);
+    copystudent.address = { streetNumber: "0000", streetName: "first", city: "somecity" };
+    return copystudent;
+}
+
+console.log(appendAddress(student));
+
+console.log(student);
+
+
+// How do we change data if the data is immutable?
+// We always produce transformed copies of the original instead of directly changing the original data.
