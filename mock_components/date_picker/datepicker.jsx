0cd808a0addd83a68f5b6c12b99745aabf09d7e3
John Kim <jkim82092@gmail.com>
2019-06-10 14:00:52 -0400

    Create different use cases of picking date/time

--- mock_components/date_picker/datepicker.jsx
+++ mock_components/date_picker/datepicker.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 class DatePickers extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            startDate: new Date()
+        };
+        
+        this.startDate = this.startDate.bind(this);
+    }
+
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
+
+    render() {
+        return (
+            <DatePicker 
+                selected={this.state.startDate} 
+                onChange={this.handleChange} 
+            /> // Select Date Only
+            <DatePicker
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={15}
+                dateFormat="h:mm aa"
+                timeCaption="Time"
+            /> // Select Time Only
+        );
+    }
 }
 
 export default DatePickers;
