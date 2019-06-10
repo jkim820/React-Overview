f7d824421d5e37274e18db0405d0d172c2a67796
John Kim <jkim82092@gmail.com>
2019-06-10 13:22:12 -0400

    Describe functionality of each attribute of  ReactCSSTransitionGroup

--- mock_components/widgets/frontend/autocomplete.jsx
+++ mock_components/widgets/frontend/autocomplete.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Autocomplete extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            inputVal: ""
+            inputValue: ""
         };
 
         this.handleInput = this.handleInput.bind(this);
@@ -14,19 +15,19 @@ class Autocomplete extends React.Component {
 
 
     handleInput(event) {
-        this.setState({ inputVal: event.currentTarget.value });
+        this.setState({ inputValue: event.currentTarget.value });
     }
 
     matches() {
         const matches = [];
-        if (this.state.inputVal.length === 0) {
+        if (this.state.inputValue.length === 0) {
             return this.props.names;
         }
         // return list of all names if there are no input values
 
         this.props.names.forEach(name => {
-            const sub = name.slice(0, this.state.inputVal.length);
-            if (sub.toLowerCase() === this.state.inputVal.toLowerCase()) {
+            const sub = name.slice(0, this.state.inputValue.length);
+            if (sub.toLowerCase() === this.state.inputValue.toLowerCase()) {
                 matches.push(name);
             }
         });
@@ -40,7 +41,7 @@ class Autocomplete extends React.Component {
 
     selectName(event) {
         const name = event.currentTarget.innerText;
-        this.setState({ inputVal: name });
+        this.setState({ inputValue: name });
     }
 
     render() {
@@ -52,14 +53,14 @@ class Autocomplete extends React.Component {
                 <h1>Autocomplete</h1>
                 <div className='auto'>
                     <input
-                        value={this.state.inputVal}
+                        value={this.state.inputValue}
                         onChange={this.handleInput}
                         placeholder='Search...' />
                     <ul>
                         <ReactCSSTransitionGroup
-                            transitionName='auto'
-                            transitionEnterTimeout={500}
-                            transitionLeaveTimeout={500}>
+                            transitionName='auto' // the name that's used to create all of the transition classes
+                            transitionEnterTimeout={500} // Specifies how long (in ms) the transition should last when the element enters the page
+                            transitionLeaveTimeout={500}> {/* Same as above, except for when an element is leaving the page */}
                             {results}
                         </ReactCSSTransitionGroup>
                         {/* names will fade in and out when entering/leaving the page  */}
