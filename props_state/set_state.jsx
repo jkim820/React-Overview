47f4d7fe1f2bea31c8871b52b26195f59ba489bd
John Kim <jkim82092@gmail.com>
2019-06-10 16:26:47 -0400

    Create another example of setState

--- props_state/set_state.jsx
+++ props_state/set_state.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
     }
 
     onClick = () => {
-        this.setState((prevState, props) => {
+        this.setState((prevState, _) => {
             return ({ counter: prevState.counter + 1 })
         }, () => console.log('callback ' + this.state.counter))
         console.log('after ' + this.state.counter)
@@ -37,3 +37,21 @@ ReactDOM.render(
 // Passing setState a function gives us two benefits.
 // 1. It allows us to take a static copy of our state that will never change on its own. 
 // 2. It will queue the setState calls so they run in order.
+
+
+
+// Example 2: increment the counter by 2
+
+onClick = () => {
+    this.setState((prevState, _) => {({ counter: prevState.counter + 1 })}
+    this.setState((prevState, _) => {({ counter: prevState.counter + 1 })}
+}
+// We pass setState a function which will guarantee both setState functions run in order
+
+
+render() {
+    console.log(this.state.counter)
+    return (
+        <button onClick={this.onClick}>Click Me!</button>
+    )
+}
