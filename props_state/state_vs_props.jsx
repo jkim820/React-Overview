86abbccc1058b9e7e5f198fc51903b21c7216e81
John Kim <jkim82092@gmail.com>
2019-06-09 21:00:23 -0400

    Differentiate between state and props

--- props_state/state_vs_props.jsx
+++ props_state/state_vs_props.jsx
@@ -1,5 +1,8 @@
 // State vs Props
 
+// Both are objects representing data that the component needs in order to render.
+
+// DIFFERENCES:
 // Props are immutable. Once set they can’t be changed.
 // State is observable. It can hold data that may change over time.
 // Props can be used in either function or class components.
@@ -26,10 +29,12 @@
 // It takes a single parameter and expects an object containing our set of values to be updated.
 // The method will update our state and then call the render() method to re-render the page.
 
-
 // Setting Multiple States
 // We’re taking our previous component state and props, as a parameter, and then we’re updating the total
 
 // this.setState((prevState, props) => ({
 //     total: prevState.count + props.diff
 // }));
+
+
+// Using Props
